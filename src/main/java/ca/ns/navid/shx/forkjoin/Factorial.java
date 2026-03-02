2b21063af4a61345e9109278b3e6d705ce60b619
Navid <navid.shx@gmail.com>
2026-03-02 11:05:51 -0500

    Factorial Done

--- src/main/java/ca/ns/navid/shx/forkjoin/Factorial.java
+++ src/main/java/ca/ns/navid/shx/forkjoin/Factorial.java
@@ -1,4 +1,39 @@
 package ca.ns.navid.shx.forkjoin;
 
-public class Factorial {
+import java.util.concurrent.RecursiveTask;
+
+public class Factorial extends RecursiveTask<Long> {
+
+    protected long number;
+
+    public Factorial(long number) {
+        this.number = number;
+    }
+
+    @Override
+    protected Long compute() {
+
+        if (this.number == 0) {
+            return this.computeSequentially();
+        }
+
+        Factorial leftTask = new Factorial(this.number - 1);
+        leftTask.fork();
+
+        Factorial rightTask = new Factorial(this.number);
+        long rightResult = rightTask.computeSequentially();
+
+        Long leftResult = leftTask.join();
+
+        return rightResult * leftResult;
+    }
+
+    private long computeSequentially() {
+
+        if (this.number == 0) {
+            return 1;
+        }
+
+        return this.number;
+    }
 }
