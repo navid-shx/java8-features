9bbcf646a1be52fc6672c2b73d1f94698fd93533
Navid <navid.shx@gmail.com>
2026-03-20 09:03:29 -0400

    Revising TimeDuration

--- src/main/java/ca/ns/navid/shx/datetime/between/TimeDuration.java
+++ src/main/java/ca/ns/navid/shx/datetime/between/TimeDuration.java
@@ -1,22 +1,29 @@
 package ca.ns.navid.shx.datetime.between;
 
 import java.time.Duration;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class TimeDuration {
 
     static void main() {
 
-        //Time Duration
-        LocalTime start = LocalTime.of(13, 45, 10);
-        LocalTime end = LocalTime.of(20, 30, 50);
+        //Duration Between LocalTimes
+        LocalTime start1 = LocalTime.of(13, 45, 10);
+        LocalTime end1 = LocalTime.of(20, 30, 50);
+        Duration duration1 = Duration.between(start1, end1);
+        IO.println("From: " + start1 + " to: " + end1);
+        IO.println("Hours: " + duration1.toHours() + "\nMinutes: " + duration1.toMinutes() + "\nSeconds: " + duration1.toSeconds());
+        IO.println(duration1.toHours() + "h " + duration1.toMinutesPart() + "m " + duration1.toSecondsPart() + "s");
 
-        Duration duration = Duration.between(start, end);
-        long hours = duration.toHours();
-        long minutes = duration.toMinutes();
-        long seconds = duration.toSeconds();
+        IO.println("\n");
 
-        IO.println("From: " + start + " to: " + end);
-        IO.println(hours + " hours and " + minutes + " minutes and " + seconds + " seconds");
+        //Duration Between LocalDateTimes
+        LocalDateTime start2 = LocalDateTime.of(2026, 1, 1, 0, 0, 0);
+        LocalDateTime end2 = LocalDateTime.of(2026, 1, 2, 10, 10, 30);
+        Duration duration2 = Duration.between(start2, end2);
+        IO.println("From: " + start2 + " to: " + end2);
+        IO.println("Hours: " + duration2.toHours() + "\nMinutes: " + duration2.toMinutes() + "\nSeconds: " + duration2.toSeconds());
+        IO.println(duration2.toHours() + "h " + duration2.toMinutesPart() + "m " + duration2.toSecondsPart() + "s");
     }
 }
